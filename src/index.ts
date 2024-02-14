ae057e2cf02d6f2a95b820827ab1f6fbc8252b3f
royisme <imroybox@gmail.com>
2024-02-13 22:14:18 -0500

    fix a bug

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import { format } from 'date-fns';
-import { slug as slugger } from "github-slugger";
+#!/usr/bin/env node
 
+import { readFileSync, writeFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, dirname} from 'path';
+import { format } from 'date-fns';
+import { slug } from 'github-slugger';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 function getDistPath() {
     const distIndex = process.argv.findIndex(arg => arg.startsWith('--dist='));
     if (distIndex > -1 && process.argv[distIndex].includes('=')) {
@@ -11,22 +15,21 @@ function getDistPath() {
     return '.'; // Use the current directory if --dist is not specified
 }
 
-
 const createPost = (title: string, distPath: string) => {
-  const slug = slugger(title);
+  const postSlug = slug(title);
   const date = format(new Date(), 'yyyy-MM-dd');
-  const templatePath = join(__dirname, '..', 'templates', 'postTemplate.md');
+  const templatePath = join(__dirname, '../src', 'templates', 'postTemplate.md');
   let content = readFileSync(templatePath, 'utf8');
 
   content = content.replace(/{{title}}/g, title).replace(/{{date}}/g, date);
 
-  const filePath = join(distPath, `${slug}.md`);
+  const filePath = join(distPath, `${postSlug}.md`);
   writeFileSync(filePath, content);
   console.log(`Post created: ${filePath}`);
 };
 
 const distPath = getDistPath(); // Get the output directory
-const title = process.argv.find(arg => !arg.startsWith('--'));
+const title = process.argv.slice(2).find(arg => !arg.startsWith('--'));
 if (!title) {
   console.error('Please provide a title for the post.');
   process.exit(1);
